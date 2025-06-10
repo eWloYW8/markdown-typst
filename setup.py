9432472bdd9b08a3d8c67ee8c621080292b85623
eWloYW8 <3171132517@qq.com>
2025-06-10 16:29:06 +0800

    feat: update action-gh-release version and bump package version to 0.1.2; add LICENSE file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="markdown-typst",
-    version="0.1.1",
+    version="0.1.2",
     author="eWloYW8",
     author_email="3171132517@qq.com",
     description="A Markdown extension to compile typst code blocks to SVG.",
